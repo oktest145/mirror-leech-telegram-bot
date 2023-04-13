a028734a3ee6024f6d58800f5cda4575ba52f6af
oktest145 <83488344+oktest145@users.noreply.github.com>
2023-04-13 09:55:11 +0530

    update bot/modules/shell.py

--- bot/modules/shell.py
+++ bot/modules/shell.py
@@ -16,6 +16,10 @@ async def shell(client, message):
     if len(cmd) == 1:
         await sendMessage(message, 'No command to execute was given.')
         return
+    blacklisted_files = ['config.env']
+    for file in blacklisted_files:
+      if file in cmd[1]:
+        await sendMessage(message,f'Using Config files not permitted')
     cmd = cmd[1]
     stdout, stderr, _ = await cmd_exec(cmd, shell=True)
     reply = ''
